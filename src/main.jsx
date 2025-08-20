148a52213b2c13596d759c3ccae01fb4b5e87317
will-i-am-12 <luwilliam2006@gmail.com>
2025-08-20 16:05:05 -0400

    add nav-bar

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './routes/Layout.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Layout/>}>
+          <Route index element = {<App/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ src/routes/Layout.jsx
@@ -0,0 +1,30 @@
+import { Outlet,Link } from "react-router-dom"
+import './Layout.css'
+const Layout =()=>
+{
+    return(
+        <div className="layout">
+            <header>
+                <div className="header-text"> 
+                    William Lu
+                </div>
+                <nav className="nav-bar">
+                    <div className="toggle-btn">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <ul className="nav-links">
+                        <li><Link to='/'>Home</Link></li>
+                        <li><Link to=''>Resume</Link></li>
+                        <li><Link to=''>Contact</Link></li>
+                    </ul>
+                </nav>
+            </header>
+            
+            <Outlet/>
+        </div>
+    )
+
+}
+export default Layout
